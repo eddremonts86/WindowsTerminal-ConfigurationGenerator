867de483819a7a982cdf9ed38019e8a8fcbc7c63
Edd Remonts <eddremonts86@gmail.com>
2020-06-17 14:18:05 +0200

    done

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,52 @@
 <template>
   <v-app>
-    <v-app-bar app color="white" dark>
+    <v-app-bar app color="white" dark flat>
       <div class="d-flex align-center">
         <v-img
           alt="Vuetify Logo"
-          class="shrink mr-2"
           contain
           src="http://eddremonts.dk/themes/thebakerdev-zenii/assets/dist/images/logo.svg?ver1"
-          transition="scale-transition"
           width="140"
         />
       </div>
 
       <v-spacer></v-spacer>
 
-      <v-btn class="primary" href="https://eddremonts.dk" target="_blank" text>
-        <span class="mr-2">Profile</span>
+      <v-btn text color="blue darken-4" href="/">
+        <span>Home</span>
+      </v-btn>
+
+      <v-btn text color="blue darken-4" href="/generator">
+        <span>Generaor</span>
+      </v-btn>
+      <v-btn
+        text
+        color="blue darken-4"
+        href="https://eddremonts.dk"
+        target="_blank"
+      >
+        <span class="mr-2">About me</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
-
     <v-content>
-      <HelloWorld transition="scale-transition" />
+      <router-view transition="scale-transition" />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
-
 export default {
   name: "App",
-
-  components: {
-    HelloWorld
-  },
-
   data: () => ({
     //
   })
 };
 </script>
+<style>
+@media (min-width: 1904px) {
+  .container {
+    max-width: 1200px !important;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,100 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <v-content class="first_row">
+      <v-container>
+        <v-row class="row_">
+          <v-col cols="6">
+            <h1 class="h1 mt-5">Windows Terminal Settings Generator</h1>
+            <h2 class="h2 mt-3">
+              Settings Generator for Windows Terminal the modern, fast,
+              efficient, powerful, and productive terminal application.
+            </h2>
+            <v-btn class="mt-10 primary" href="/generator">
+              Gnerate settings
+            </v-btn>
+          </v-col>
+
+          <v-col cols="6">
+            <img
+              class="terminal_img"
+              src="https://www.softzone.es/app/uploads-softzone.es/2020/05/Windows-Terminal-con-efecto-acr%C3%ADlico-800x419.jpg"
+              width="100%"
+            />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+    <v-content class="">
+      <v-container>
+        <v-row class="row_">
+          <v-col cols="12 center">
+            <h1 class="h1 ma-10">What is Windows Terminal?</h1>
+          </v-col>
+          <v-col cols="6">
+            <img
+              src="https://2vplsu2wo1292qcj331fo1gj-wpengine.netdna-ssl.com/wp-content/uploads/2019/06/windows_terminal_icon-1-728x409.jpg"
+              width="100%"
+            />
+          </v-col>
+          <v-col cols="6">
+            <v-card>
+              <v-card-text>
+                <div class="card_text">
+                  <p>
+                    The Windows Terminal is a modern, fast, efficient, powerful,
+                    and productive terminal application for users of
+                    command-line tools and shells like Command Prompt,
+                    PowerShell, and WSL.
+                  </p>
+                  <p>
+                    Its main features include multiple tabs, panes, Unicode and
+                    UTF-8 character support, a GPU accelerated text rendering
+                    engine, and custom themes, styles, and configurations.
+                  </p>
+                  <p>
+                    This is an open source project and we welcome community
+                    participation. To participate please visit
+                    https://github.com/microsoft/terminal
+                  </p>
+                </div>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
-};
+export default { name: "Home" };
 </script>
+
+<style>
+.h1 {
+  font-size: 3rem;
+  font-weight: 900;
+  line-height: 3.5rem;
+}
+.h2,
+.card_text {
+  font-size: 1.2rem;
+  font-weight: 400;
+}
+.card_text {
+  line-height: 2rem;
+}
+.terminal_img {
+  border-radius: 8px;
+}
+.first_row {
+  background-color: #cccccc3b;
+}
+.center {
+  text-align: center;
+}
+.row_ {
+  padding-bottom: 50px;
+  padding-top: 50px;
+}
+</style>
